a292199ad92fecbe1295a1b67b99ceba5e62fdbd
=someone2317 <=samherrera2016@icloud.com>
2021-01-16 11:24:46 -0600

    changes

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-#include "Robot.h"
-
-#include <iostream>
-
-#include <frc/smartdashboard/SmartDashboard.h>
-
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-}
-
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
-=======
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -36,37 +15,11 @@ void Robot::RobotInit() {}
 /**
  * This function is called every robot packet, no matter the mode. Use
  * this for items like diagnostics that you want to run during disabled,
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
  * autonomous, teleoperated and test.
  *
  * <p> This runs after the mode specific periodic functions, but before
  * LiveWindow and SmartDashboard integrated updating.
  */
-<<<<<<< HEAD
-void Robot::RobotPeriodic() {}
-
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-=======
 // this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
 // this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
 // this is a whole bunch of new code I added to my personal branch that I wanna copy over eventually
@@ -112,36 +65,10 @@ void Robot::AutonomousInit() {
 
   if (m_autonomousCommand != nullptr) {
     m_autonomousCommand->Schedule();
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
   }
 }
 
 void Robot::AutonomousPeriodic() {
-<<<<<<< HEAD
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::TeleopInit() {}
-
-void Robot::TeleopPeriodic() {}
-
-void Robot::DisabledInit() {}
-
-void Robot::DisabledPeriodic() {}
-
-void Robot::TestInit() {}
-
-void Robot::TestPeriodic() {}
-
-#ifndef RUNNING_FRC_TESTS
-int main() {
-  return frc::StartRobot<Robot>();
-}
-=======
 
 }
 
@@ -183,5 +110,4 @@ void Robot::TestPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
 int main() { return frc::StartRobot<Robot>(); }
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
 #endif
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
-#pragma once
-
-#include <string>
-
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
-=======
 /*----------------------------------------------------------------------------*/
 /* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
@@ -23,33 +11,17 @@
 #include <frc2/command/Command.h>
 
 #include "RobotContainer.h"
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
 
 class Robot : public frc::TimedRobot {
  public:
   void RobotInit() override;
   void RobotPeriodic() override;
-<<<<<<< HEAD
-=======
   void DisabledInit() override;
   void DisabledPeriodic() override;
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
   void AutonomousInit() override;
   void AutonomousPeriodic() override;
   void TeleopInit() override;
   void TeleopPeriodic() override;
-<<<<<<< HEAD
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void TestInit() override;
-  void TestPeriodic() override;
-
- private:
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
-=======
   void TestPeriodic() override;
 
  private:
@@ -58,5 +30,4 @@ class Robot : public frc::TimedRobot {
   frc2::Command* m_autonomousCommand = nullptr;
 
   RobotContainer m_container;
->>>>>>> 119e8b76d8cbbb98afe2ba5b6ee254a4c9262844
 };
